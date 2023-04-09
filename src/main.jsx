13cce3e12ac9e7cef931a8a2ba59518f34e87bc5
mdashraful <mdashrafulislam369@gmail.com>
2023-04-10 02:50:56 +0600

    initial setup and tailwind install and react router added

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import ErrorPage from './error-page'
+import Root from './Root'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: < ErrorPage />
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
