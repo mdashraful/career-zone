b50f49b1fad64aa821b07f3986af489395052969
mdashraful <mdashrafulislam369@gmail.com>
2023-04-12 00:14:09 +0600

    added indivual banner

--- src/components/SinglePageBanner/SinglePageBanner.jsx
+++ src/components/SinglePageBanner/SinglePageBanner.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const SinglePageBanner = ({ children }) => {
     return (
         <div className='h-56 flex items-center justify-center'>
-            <h1 className='text-3xl font-extrabold'>{children}</h1>
+            <h1 className='text-3xl font-bold'>{children}</h1>
         </div>
     );
 };
