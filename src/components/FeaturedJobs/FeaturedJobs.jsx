ba19b02b2d2f366c93a1d293d1e08992aa54e67a
unknown <mdashrafulislam369@gmail.com>
2023-04-11 13:04:39 +0600

    featured job component added

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 const FeaturedJobs = () => {
     return (
-        <div>
-            FeaturedJobs
+        <div className='md:px-24 px-5 m-auto pb-28'>
+            <div className='text-center mb-7'>
+                <h2 className='text-5xl font-bold mb-3'>Featured Jobs</h2>
+                <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
+            </div>
         </div>
     );
 };
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,56 +1,60 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import FeaturedJobs from '../FeaturedJobs/FeaturedJobs';
 
 const Home = () => {
     const data = useLoaderData();
     console.log(data);
     return (
-        <div className='md:px-24 px-5 m-auto py-28'>
-            <div className='text-center mb-7'>
-                <h2 className='text-5xl font-bold mb-3'>Job Category List</h2>
-                <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
+        <div>
+            <div className='md:px-24 px-5 m-auto py-28'>
+                <div className='text-center mb-7'>
+                    <h2 className='text-5xl font-bold mb-3'>Job Category List</h2>
+                    <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
+                </div>
+                <div className='grid lg:grid-cols-4 gap-5 md:grid-cols-2'>
+                    <div className='bg-gray-50 p-8 rounded-md'>
+                        <div className='mb-5 '>
+                            <img className='bg-gray-100 p-2 rounded' src="/public/Icons/accounts-1.png" alt="" />
+                        </div>
+                        <div>
+                            <h6 className='font-bold text-xl mb-1'>300 Jobs Available</h6>
+                            <p>300 Jobs Available</p>
+                        </div>
+                    </div>
+
+                    <div className='bg-gray-50 p-8 rounded-md'>
+                        <div className='mb-5'>
+                            <img className='bg-gray-100 p-2 rounded' src="/public/Icons/business-1.png" alt="" />
+                        </div>
+                        <div>
+                            <h6 className='font-bold text-xl mb-1'>Creative Design</h6>
+                            <p>100+ Jobs Available</p>
+                        </div>
+                    </div>
+
+                    <div className='bg-gray-50 p-8 rounded-md'>
+                        <div className='mb-5'>
+                            <img className='bg-gray-100 p-2 rounded' src="/public/Icons/chip-1.png" alt="" />
+                        </div>
+                        <div>
+                            <h6 className='font-bold text-xl mb-1'>Marketing & Sales</h6>
+                            <p>150 Jobs Available</p>
+                        </div>
+                    </div>
+
+                    <div className='bg-gray-50 p-8 rounded-md'>
+                        <div className='mb-5'>
+                            <img className='bg-gray-100 p-2 rounded' src="/public/Icons/social-media-1.png" alt="" />
+                        </div>
+                        <div>
+                            <h6 className='font-bold text-xl mb-1'>Engineering Job</h6>
+                            <p>224 Jobs Available</p>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className='columns-1 md:columns-2 lg:columns-4 gap-4'>
-                <div className='bg-gray-50 p-8 rounded-md'>
-                    <div className='mb-5 '>
-                        <img className='bg-gray-100 p-2 rounded' src="/public/Icons/accounts-1.png" alt="" />
-                    </div>
-                    <div>
-                        <h6 className='font-bold text-xl mb-1'>300 Jobs Available</h6>
-                        <p>300 Jobs Available</p>
-                    </div>
-                </div>
-
-                <div className='bg-gray-50 p-8 rounded-md'>
-                    <div className='mb-5'>
-                        <img className='bg-gray-100 p-2 rounded' src="/public/Icons/business-1.png" alt="" />
-                    </div>
-                    <div>
-                        <h6 className='font-bold text-xl mb-1'>Creative Design</h6>
-                        <p>100+ Jobs Available</p>
-                    </div>
-                </div>
-
-                <div className='bg-gray-50 p-8 rounded-md'>
-                    <div className='mb-5'>
-                        <img className='bg-gray-100 p-2 rounded' src="/public/Icons/chip-1.png" alt="" />
-                    </div>
-                    <div>
-                        <h6 className='font-bold text-xl mb-1'>Marketing & Sales</h6>
-                        <p>150 Jobs Available</p>
-                    </div>
-                </div>
-
-                <div className='bg-gray-50 p-8 rounded-md'>
-                    <div className='mb-5'>
-                        <img className='bg-gray-100 p-2 rounded' src="/public/Icons/social-media-1.png" alt="" />
-                    </div>
-                    <div>
-                        <h6 className='font-bold text-xl mb-1'>Engineering Job</h6>
-                        <p>224 Jobs Available</p>
-                    </div>
-                </div>
-            </div>
+            <FeaturedJobs></FeaturedJobs>
         </div>
     );
 };
